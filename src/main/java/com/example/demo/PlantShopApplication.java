db8d0e504ad0c6128255ec8cb3ba52a52a9b87da
Gagandeep Kaur <kgagandeep@vmware.com>
2021-07-29 10:13:34 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/PlantShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantShopApplication.class, args);
+	}
+
+}
